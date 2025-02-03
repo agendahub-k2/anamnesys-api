7de47d146f3222b71e9ba1954999d2a9eb9fd22d
idd-iqueiroz <idd_iqueiroz@uolinc.com>
2025-02-03 14:04:22 -0300

    Removendo tests

--- src/test/java/com/anamnesys/AnamnesysApiApplicationTests.java
+++ src/test/java/com/anamnesys/AnamnesysApiApplicationTests.java
@@ -3,10 +3,10 @@ package com.anamnesys;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AnamnesysApiApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
