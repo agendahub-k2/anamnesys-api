3d9d135155cc5cebbf6752980464561a694c191c
idd-iqueiroz <idd_iqueiroz@uolinc.com>
2025-02-07 19:40:18 -0300

    add cors

--- src/main/java/com/anamnesys/config/SecurityConfig.java
+++ src/main/java/com/anamnesys/config/SecurityConfig.java
@@ -45,7 +45,6 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
-
 }
 
 
